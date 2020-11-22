a427cebfa5c96398a06c2414c1c21ac67e2bbb0e
Obinna Ezekwe <ozekwe@gmail.com>
2020-11-21 21:45:58 -0600

    Created my readme file template

--- Readme.md
+++ Readme.md
@@ -1,2 +1,37 @@
-# Run-Buddy
+# 
 
+  ## Description
+      
+
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contribute)
+  
+  
+  ## Installation
+      
+
+
+  ## Usage
+      
+
+
+  ## License
+      GNU
+
+
+  ## Contributing
+      
+
+
+  ## Tests
+      
+
+
+  ## Questions
+  Github profile: [oezekwe](https://github.com/oezekwe)
+  
+  Contact: ozekwe@gmail.com
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,41 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+      ${data.description}
+
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contribute)
+  
+  
+  ## Installation
+      ${data.installation}
+
+
+  ## Usage
+      ${data.usage}
+
+
+  ## License
+      ${data.license}
+
+
+  ## Contributing
+      ${data.contribute}
+
+
+  ## Tests
+      ${data.tests}
+
+
+  ## Questions
+  Github profile: [${data.gitName}](https://github.com/${data.gitName})
+  
+  Contact: ${data.email}
 `;
 }
 
